383200e03d5543298f6bbe87ddf037e829ea36ff
Lee Sangdae <mousesd@gmail.com>
2018-08-11 16:48:56 +0900

    Rule #2: Consider a builder when faced with many constructor parameters

--- Rule02/src/net/homenet/rule02/AppMain.java
+++ Rule02/src/net/homenet/rule02/AppMain.java
@@ -1,5 +1,13 @@
 package net.homenet.rule02;
 
 public class AppMain {
+    public static void main(String[] args) {
+        NutritionFact cocaCola = new NutritionFact.Builder(240, 8)
+            .setCalories(100)
+            .setSodium(35)
+            .setCarbohydrate(27)
+            .Build();
 
+        System.out.println(cocaCola);
+    }
 }
---
+++ Rule02/src/net/homenet/rule02/NutritionFact.java
@@ -0,0 +1,69 @@
+package net.homenet.rule02;
+
+@SuppressWarnings({"WeakerAccess", "FieldCanBeLocal", "unused"})
+public class NutritionFact {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public NutritionFact(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    @Override
+    public String toString() {
+        return "servingSize: " + servingSize
+            + ", servings: " + servings
+            + ", calories: " + calories
+            + ", fat: " + fat
+            + ", sodium: " + sodium
+            + ", carbohydrate: " + carbohydrate;
+    }
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder setCalories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder setFat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder setSodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder setCarbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFact Build() {
+            return new NutritionFact(this);
+        }
+    }
+}
